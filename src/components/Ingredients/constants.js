338e56420cfae9652b0ac17064e62c0c67f8947f
ZachMac <zachtball@gmail.com>
2020-12-05 21:37:52 -0700

    changed modal logic

---
+++ src/components/Ingredients/constants.js
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-anonymous-default-export */
+/*
+  disabling eslint rule becuase:
+  you can use this as a global object for any useReducer functions, therefore you shouldn't name it
+*/
+export default {
+  set: 'SET',
+  add: 'ADD',
+  delete: 'DELETE',
+};
